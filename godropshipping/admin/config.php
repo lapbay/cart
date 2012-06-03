42c0f116d9324c77dab4a986dc65f6dc7d1c4bf5
Milan <wcmilan@gmail.com>
2012-06-03 22:51:32 +0800

    first running version

---
+++ godropshipping/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/');
+define('HTTP_IMAGE', 'http://localhost:8080/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/admin/');
+define('HTTPS_IMAGE', 'http://localhost:8080/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/milan/git/cart/godropshipping/admin/');
+define('DIR_SYSTEM', '/Users/milan/git/cart/godropshipping/system/');
+define('DIR_DATABASE', '/Users/milan/git/cart/godropshipping/system/database/');
+define('DIR_LANGUAGE', '/Users/milan/git/cart/godropshipping/admin/language/');
+define('DIR_TEMPLATE', '/Users/milan/git/cart/godropshipping/admin/view/template/');
+define('DIR_CONFIG', '/Users/milan/git/cart/godropshipping/system/config/');
+define('DIR_IMAGE', '/Users/milan/git/cart/godropshipping/image/');
+define('DIR_CACHE', '/Users/milan/git/cart/godropshipping/system/cache/');
+define('DIR_DOWNLOAD', '/Users/milan/git/cart/godropshipping/download/');
+define('DIR_LOGS', '/Users/milan/git/cart/godropshipping/system/logs/');
+define('DIR_CATALOG', '/Users/milan/git/cart/godropshipping/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'gds');
+define('DB_PASSWORD', 'gdsPassword123');
+define('DB_DATABASE', 'gds');
+define('DB_PREFIX', 'gds');
+?>
---
+++ godropshipping/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/');
+define('HTTP_IMAGE', 'http://localhost:8080/image/');
+define('HTTP_ADMIN', 'http://localhost:8080/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/');
+define('HTTPS_IMAGE', 'http://localhost:8080/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/milan/git/cart/godropshipping/catalog/');
+define('DIR_SYSTEM', '/Users/milan/git/cart/godropshipping/system/');
+define('DIR_DATABASE', '/Users/milan/git/cart/godropshipping/system/database/');
+define('DIR_LANGUAGE', '/Users/milan/git/cart/godropshipping/catalog/language/');
+define('DIR_TEMPLATE', '/Users/milan/git/cart/godropshipping/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/milan/git/cart/godropshipping/system/config/');
+define('DIR_IMAGE', '/Users/milan/git/cart/godropshipping/image/');
+define('DIR_CACHE', '/Users/milan/git/cart/godropshipping/system/cache/');
+define('DIR_DOWNLOAD', '/Users/milan/git/cart/godropshipping/download/');
+define('DIR_LOGS', '/Users/milan/git/cart/godropshipping/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'gds');
+define('DB_PASSWORD', 'gdsPassword123');
+define('DB_DATABASE', 'gds');
+define('DB_PREFIX', 'gds');
+?>
